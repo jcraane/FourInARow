f6a448b2bdd962af6fce3c4281e2488dc18ec8a8
jamiecraane <jcraane@anwb.nl>
2022-06-27 23:19:09 +0200

    Add winner row

--- src/main/kotlin/dev/jamiecraane/Main.kt
+++ src/main/kotlin/dev/jamiecraane/Main.kt
@@ -1,8 +1,12 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -17,16 +21,28 @@ import dev.jamiecraane.util.ViewModel
 @Preview
 fun App() {
     val scope = rememberCoroutineScope()
-    val viewModel = ViewModel().init(scope)
+    val viewModel = ViewModel().apply {
+        init(scope)
+    }
+    val gameState by viewModel.gameBoard.gameStatusFlow.collectAsState(null)
 
     FourInARowTheme {
-        Board(
-            numColumns = FourInARow.NUM_COLS,
-            numRows = FourInARow.NUM_ROWS,
-            onClickListener = {column ->
-                println("Add new piece at column $column")
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row() {
+                Text(text = "Winner: ")
+                gameState?.winner?.let {
+                    Text(text = it.name)
+                }
             }
-        )
+            Spacer(modifier = Modifier.height(12.dp))
+            Board(
+                numColumns = FourInARow.NUM_COLS,
+                numRows = FourInARow.NUM_ROWS,
+                onClickListener = {column ->
+                    println("Add new piece at column $column")
+                }
+            )
+        }
     }
 }
 
--- src/main/kotlin/dev/jamiecraane/util/ViewModel.kt
+++ src/main/kotlin/dev/jamiecraane/util/ViewModel.kt
@@ -6,7 +6,8 @@ import kotlinx.coroutines.CoroutineScope
 open class ViewModel {
     private lateinit var viewModelScope: CoroutineScope
 
-    private var gameBoard = FourInARow()
+    var gameBoard = FourInARow()
+        private set
 
     open fun init(viewModelScope: CoroutineScope) {
         this.viewModelScope = viewModelScope
